8fba4ee75dcce2f0a5c1062867a427aabc1ff4a6
Lisa Sam Wang <sam_wang_lisa1121@berkeley.edu>
2021-08-06 20:05:54 -0700

    tests

--- proj2_test.go
+++ proj2_test.go
@@ -126,3 +126,142 @@ func TestShare(t *testing.T) {
 		return
 	}
 }
+
+func TestReceiveAfterRevokeError(t *testing.T) {
+	//call our own functions to see if it works. Should be similar to performing these things on a regular terminal.
+	clear()
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+
+	var v2 []byte
+	var accessToken uuid.UUID
+
+	v, err = u.LoadFile("file1")
+	if err != nil {
+		t.Error("Failed to download the file from alice", err)
+		return
+	}
+
+	accessToken, err = u.ShareFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	err = u.RevokeFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to revoke access to a file", err)
+	}
+
+	err = u2.ReceiveFile("file2", "alice", accessToken)
+	if err == nil {
+		t.Error("Revoke File did not work", err)
+		return
+	}
+
+	v2, err = u2.LoadFile("file2")
+	if err == nil {
+		t.Error("File was downloaded after sharing, but it wasn't supposed to share in the first place", err)
+		return
+	}
+	if !reflect.DeepEqual(v, v2) {
+		t.Error("The files shared and received were not the same", v, v2)
+		return
+	}
+	//testing malicious bob
+	//alice shares w bob
+	//bob is bad. bob does not call receivefile.
+	//alice realizes bob is bad. alice revokes access
+	//bob calls receivefile now. this should error
+	//alice.sharefile(secret, bob)
+	//alice.revokefile(secret, bob)
+	//bob.receivefile(secret, alice)
+	//should error
+}
+
+func TestAppendsFile(t *testing.T) {
+	clear()
+	u, err := InitUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to initialize user", err)
+		return
+	}
+	u2, err2 := InitUser("bob", "foobar")
+	if err2 != nil {
+		t.Error("Failed to initialize bob", err2)
+		return
+	}
+
+	v := []byte("This is a test")
+	u.StoreFile("file1", v)
+
+	var v2 []byte
+	var accessToken uuid.UUID
+
+	v, err = u.LoadFile("file1")
+	if err != nil {
+		t.Error("Failed to download the file from alice", err)
+		return
+	}
+
+	accessToken, err = u.ShareFile("file1", "bob")
+	if err != nil {
+		t.Error("Failed to share the a file", err)
+		return
+	}
+	err = u2.ReceiveFile("file2", "alice", accessToken)
+	if err != nil {
+		t.Error("Failed to receive the share message", err)
+		return
+	}
+
+	v2, err = u2.LoadFile("file2")
+	if err != nil {
+		t.Error("Failed to download the file after sharing", err)
+		return
+	}
+	if !reflect.DeepEqual(v, v2) {
+		t.Error("Shared file is not the same", v, v2)
+		return
+	}
+
+	err = u2.AppendFile("file2", v2)
+	if err != nil {
+		t.Error("Append File failed", err)
+	}
+
+	err = u2.AppendFile("file3", v2)
+	if err == nil {
+		t.Error("Appending to a nonexistent file did not fail", err)
+	}
+
+	if !reflect.DeepEqual(v, v2) {
+		t.Error("The files changed were not the same (when they should be)", v, v2)
+	}
+
+	v3 := []byte("Hello World, random text @/$@jasdnajsnc random junk appends")
+
+	err = u.AppendFile("file1", v3)
+	if err != nil {
+		t.Error("Alice failed to append to her own file", err)
+	}
+
+	if !reflect.DeepEqual(v, v2) {
+		t.Error("After Alice appended, the files are no longer equal", v, v2)
+	}
+
+}
+
+func TestChainSharing(t *testing.T) {
+
+}
